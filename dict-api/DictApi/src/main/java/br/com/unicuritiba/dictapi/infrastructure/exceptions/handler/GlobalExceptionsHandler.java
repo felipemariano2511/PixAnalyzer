6e491c23dc094d328bf7e564478c9fac463fedca
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-05 21:19:01 -0300

    🐛 fix : Corrigindo endereçamento de pacotes

--- dict-api/DictApi/src/main/java/br/com/unicuritiba/dictapi/infrastructure/exceptions/handler/GlobalExceptionsHandler.java
+++ dict-api/DictApi/src/main/java/br/com/unicuritiba/dictapi/infrastructure/exceptions/handler/GlobalExceptionsHandler.java
@@ -1,6 +1,6 @@
 package br.com.unicuritiba.dictapi.infrastructure.exceptions.handler;
 
-import br.com.unicuritiba.projetoathus.infrastructure.exceptions.*;
+import br.com.unicuritiba.dictapi.infrastructure.exceptions.*;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
