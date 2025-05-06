8ec02924b4ef86d9a4a406615cf5832c855be22d
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-05 23:03:46 -0300

    🐛 fix : Corrigindo response para o padrão do DTO

--- registro-nacional/DadosGov/src/main/java/br/com/unicuritiba/dadosgov/dto/DataDTO.java
+++ registro-nacional/DadosGov/src/main/java/br/com/unicuritiba/dadosgov/dto/DataDTO.java
@@ -3,21 +3,17 @@ package br.com.unicuritiba.dadosgov.dto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Getter;
 import lombok.Setter;
-
 import java.util.Date;
-import java.util.UUID;
 
 @Getter
 @Setter
 public class DataDTO {
 
-    private UUID id;
     private String cpf;
     private String fullName;
 
     @JsonFormat(pattern = "yyyy-MM-dd")
     private Date birthDate;
-
     private Date registrationDate;
     private String status;
     private String checkDigits;
--- registro-nacional/DadosGov/src/main/java/br/com/unicuritiba/dadosgov/services/DataService.java
+++ registro-nacional/DadosGov/src/main/java/br/com/unicuritiba/dadosgov/services/DataService.java
@@ -1,8 +1,10 @@
 package br.com.unicuritiba.dadosgov.services;
 
+import br.com.unicuritiba.dadosgov.domain.models.Data;
+import br.com.unicuritiba.dadosgov.dto.DataDTO;
 import br.com.unicuritiba.dadosgov.infrastructure.exceptions.NotFoundException;
-
 import br.com.unicuritiba.dadosgov.domain.repositories.DataRepository;
+import br.com.unicuritiba.dadosgov.mappers.DataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -15,9 +17,13 @@ public class DataService {
     private DataRepository repository;
 
 
-    public ResponseEntity<?> getByCpf(String cpf) {
-        return ResponseEntity.ok(repository.findByCpf(cpf)
-                .orElseThrow(()-> new NotFoundException(String.format("Não encontrado CPF %s em nossa base de dados.", cpf))));
+    public ResponseEntity<DataDTO> getByCpf(String cpf) {
+        var cpfData = repository.findByCpf(cpf)
+                .orElseThrow(() -> new NotFoundException(
+                        String.format("o CPF %s não foi encontrado em nossa base de dados.", cpf)
+                ));
+
+        return ResponseEntity.ok(DataMapper.toDTO(cpfData));
     }
 
 }
