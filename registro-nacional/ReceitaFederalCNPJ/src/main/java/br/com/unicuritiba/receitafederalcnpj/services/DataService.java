57d80cf40b09bb0f918a6143704fc33f9a5f9b67
Vinícius Stencel Canto Silva <stencel.vinicius@gmail.com>
2025-05-07 21:38:33 -0300

    ♻️ refactor : Arrumando DTO e Service para retornar JSON formatado corretamente

--- registro-nacional/ReceitaFederalCNPJ/src/main/java/br/com/unicuritiba/receitafederalcnpj/services/DataService.java
+++ registro-nacional/ReceitaFederalCNPJ/src/main/java/br/com/unicuritiba/receitafederalcnpj/services/DataService.java
@@ -3,18 +3,23 @@ package br.com.unicuritiba.receitafederalcnpj.services;
 import br.com.unicuritiba.dadosgov.infrastructure.exceptions.NotFoundException;
 import br.com.unicuritiba.receitafederalcnpj.domain.models.Data;
 import br.com.unicuritiba.receitafederalcnpj.domain.repositories.DataRepository;
+import br.com.unicuritiba.receitafederalcnpj.dto.DataResponseDTO;
+import br.com.unicuritiba.receitafederalcnpj.mappers.DataMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DataService {
+
     @Autowired
     private DataRepository repository;
 
-    public ResponseEntity<Data> searchCnpj(String cnpj) {
-        return ResponseEntity.ok(repository.findByCnpj(cnpj)
-                .orElseThrow(() -> new NotFoundException(String.format("CNPJ %s não encontrado no Banco de Dados.",cnpj))));
-    }
+    public ResponseEntity<DataResponseDTO> searchCnpj(String cnpj) {
+        Data data = repository.findByCnpj(cnpj)
+                .orElseThrow(() -> new NotFoundException(String.format("CNPJ %s não encontrado no Banco de Dados.", cnpj)));
 
+        DataResponseDTO dto = DataMapper.toResponseDTO(data);
+        return ResponseEntity.ok(dto);
+    }
 }
