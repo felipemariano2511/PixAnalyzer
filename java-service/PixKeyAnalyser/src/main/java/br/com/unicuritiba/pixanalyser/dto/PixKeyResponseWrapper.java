d7e5634188b921a5e1d450c63c92d93e23fe00e4
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-07 23:33:23 -0300

    ✨ feat (DTO) : Criação de DTOs para padronização de dados

---
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/dto/PixKeyResponseWrapper.java
@@ -0,0 +1,13 @@
+package br.com.unicuritiba.pixanalyser.dto;
+
+public class PixKeyResponseWrapper {
+    private PixKeyResponse body;
+
+    public PixKeyResponse getBody() {
+        return body;
+    }
+
+    public void setBody(PixKeyResponse body) {
+        this.body = body;
+    }
+}
