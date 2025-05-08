132ef450396a030093db6ecf40787e578f8b8d50
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-07 22:05:59 -0300

    🐛 fix : Correção de endereçamento de packeges

--- registro-nacional/ReceitaFederalCNPJ/src/main/java/br/com/unicuritiba/receitafederalcnpj/infrastructure/exceptions/handler/ErrorResponse.java
+++ registro-nacional/ReceitaFederalCNPJ/src/main/java/br/com/unicuritiba/receitafederalcnpj/infrastructure/exceptions/handler/ErrorResponse.java
@@ -1,16 +1,13 @@
-package br.com.unicuritiba.dadosgov.infrastructure.exceptions.handler;
+package br.com.unicuritiba.receitafederalcnpj.infrastructure.exceptions.handler;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @NoArgsConstructor
-
-
 public class ErrorResponse {
 
     private LocalDateTime timestamp;
