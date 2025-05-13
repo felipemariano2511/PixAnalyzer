4cfafd3285b7cfd9319ad3be09b6eda153c41774
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-13 14:41:52 -0300

    📝 data :  Criando DTOs e Mappers para manipulação de dados

---
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/dto/PixKeyResponseWrapperDto.java
@@ -0,0 +1,11 @@
+package br.com.unicuritiba.pixanalyser.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+public class PixKeyResponseWrapperDto {
+    private DictApiResponseDto body;
+
+}
