81a279e5a30947ad316bd1231be02d610c515675
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-13 14:43:40 -0300

    ♻️ refeactor : Removendo propriedades não utilizadas

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/domain/models/PixTransaction.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/domain/models/PixTransaction.java
@@ -17,27 +17,20 @@ public class PixTransaction {
     @GeneratedValue
     private UUID id;
 
-    @Column(name = "end_to_end_id", nullable = false, unique = true)
     private String endToEndId;
 
-    @Column(name = "origin_client_id", nullable = false)
     private Long originClientId;
 
-    @Column(name = "origin_bank", nullable = false, length = 20)
     private String originBank;
 
-    @Column(name = "destination_key_value", nullable = false)
     private String destinationKeyValue;
 
-    @Column(name = "destination_bank", nullable = false)
     private String destinationBank;
 
     @Column(name = "amount", nullable = false, precision = 15, scale = 2)
     private BigDecimal amount;
 
-    @Column(name = "description")
     private String description;
 
-    @Column(name = "timestamp", nullable = false)
     private LocalDateTime timestamp;
 }
