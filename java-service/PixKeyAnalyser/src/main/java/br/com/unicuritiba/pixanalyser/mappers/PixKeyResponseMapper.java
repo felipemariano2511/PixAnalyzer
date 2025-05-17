6e6a14ebf3a94c2cf2dac54da11d8eac4687482e
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-17 17:00:09 -0300

    ✨ feat (gRPC) : Incluindo os campos retornados pela IA para o retorno do usuário

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/mappers/PixKeyResponseMapper.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/mappers/PixKeyResponseMapper.java
@@ -2,22 +2,31 @@ package br.com.unicuritiba.pixanalyser.mappers;
 
 import br.com.unicuritiba.pixanalyser.domain.models.PixTransaction;
 import br.com.unicuritiba.pixanalyser.dto.PixKeyResponseDto;
-
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 
 public class PixKeyResponseMapper {
 
-    public static PixKeyResponseDto toResponseDto(PixTransaction transaction, String receiverName) {
+    public static PixKeyResponseDto toResponseDto(PixTransaction transaction, String receiverName, double confidenceScore, List<String> fraudReasons) {
+        PixKeyResponseDto.Transfer transfer = new PixKeyResponseDto.Transfer();
+        transfer.setEndToEndId(transaction.getEndToEndId());
+        transfer.setOriginClientId(transaction.getOriginClientId());
+        transfer.setOriginBank(transaction.getOriginBank());
+        transfer.setDestinationKeyValue(transaction.getDestinationKeyValue());
+        transfer.setReceiverName(receiverName);
+        transfer.setDestinationBank(transaction.getDestinationBank());
+        transfer.setAmount(transaction.getAmount().toPlainString());
+        transfer.setDescription(transaction.getDescription());
+        transfer.setTimestamp(transaction.getTimestamp().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+
+        PixKeyResponseDto.AiAnalyse analysis = new PixKeyResponseDto.AiAnalyse();
+        analysis.setConfidenceScore(confidenceScore);
+        analysis.setFraudReasons(fraudReasons);
+
         PixKeyResponseDto response = new PixKeyResponseDto();
-        response.setEndToEndId(transaction.getEndToEndId());
-        response.setOriginClientId(transaction.getOriginClientId());
-        response.setOriginBank(transaction.getOriginBank());
-        response.setDestinationKeyValue(transaction.getDestinationKeyValue());
-        response.setReceiverName(receiverName);
-        response.setDestinationBank(transaction.getDestinationBank());
-        response.setAmount(transaction.getAmount().toPlainString());
-        response.setDescription(transaction.getDescription());
-        response.setTimestamp(transaction.getTimestamp().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
+        response.setTransfers(List.of(transfer));
+        response.setAnalysis(analysis);
+
         return response;
     }
 }
