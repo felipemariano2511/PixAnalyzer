6184f62dfc8ef4306b3f5e84d1038412cb7bd8af
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-22 00:23:53 -0300

    ✨ feat : Inserindo métodos de consulta de dados no banco de dados

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/domain/repositories/PixTransactionRepository.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/domain/repositories/PixTransactionRepository.java
@@ -4,4 +4,7 @@ import br.com.unicuritiba.pixanalyser.domain.models.PixTransaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.UUID;
 
-public interface PixTransactionRepository extends JpaRepository<PixTransaction, UUID> {}
+public interface PixTransactionRepository extends JpaRepository<PixTransaction, UUID> {
+    int countByOriginClientIdAndDestinationKeyValue(Long originClientId, String destinationKeyValue);
+    int countByDestinationKeyValue(String destinationKeyValue);
+}
