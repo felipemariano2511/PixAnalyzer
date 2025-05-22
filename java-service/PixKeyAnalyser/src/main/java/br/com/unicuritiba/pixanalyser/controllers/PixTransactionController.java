9e4d211ab59602d93d593d1f2efb872ea3800682
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-22 00:25:40 -0300

    ✨ feat : Criando novas rotas para melhorar as regras de negócio da aplicação

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/controllers/PixTransactionController.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/controllers/PixTransactionController.java
@@ -1,26 +1,47 @@
 package br.com.unicuritiba.pixanalyser.controllers;
 
+import br.com.unicuritiba.pixanalyser.application.services.AiAnalyzeService;
+import br.com.unicuritiba.pixanalyser.application.services.InfoTransactionService;
 import br.com.unicuritiba.pixanalyser.application.services.PixTransactionService;
+import br.com.unicuritiba.pixanalyser.dto.AiAnalyzeRequestDto;
 import br.com.unicuritiba.pixanalyser.dto.PixTransactionRequestDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/transferencia/pix")
 public class PixTransactionController {
 
     @Autowired
-    private PixTransactionService service;
+    private PixTransactionService transactionService;
 
-    @PostMapping
-    public ResponseEntity<?> makePixTransfer(@RequestBody PixTransactionRequestDto body) {
-        return ResponseEntity.ok(service.createTransaction(
+    @Autowired
+    private InfoTransactionService infoTransactionService;
+
+    @Autowired
+    private AiAnalyzeService aiAnalyzeService;
+
+    @PostMapping("/info-chave-pix")
+    public ResponseEntity<?> fetchTransactionInformation(@RequestBody PixTransactionRequestDto body) {
+        return ResponseEntity.ok(infoTransactionService.fetchTransactionInformation(
                 body.getDestinationKeyValue(),
-                body.getIdClient(),
+                body.getOriginClientId()));
+    }
+
+    @PostMapping("/analisar")
+    public ResponseEntity<?> aiAnalizePixKey(@RequestBody AiAnalyzeRequestDto body) {
+        return ResponseEntity.ok(aiAnalyzeService.aiAnalyzePixKey(
+                body.getDestinationKeyValue(),
+                body.getOriginClientId(),
+                body));
+    }
+
+    @PostMapping("/transferir")
+    public ResponseEntity<?> makePixTransfer(@RequestBody PixTransactionRequestDto body) {
+        return ResponseEntity.ok(transactionService.createTransaction(
+                body.getDestinationKeyValue(),
+                body.getOriginClientId(),
                 body.getAmount(),
                 body.getDescription()));
     }
