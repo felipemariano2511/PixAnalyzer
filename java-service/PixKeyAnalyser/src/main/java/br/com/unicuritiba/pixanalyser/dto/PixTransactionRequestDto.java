77c5acd663fcee10aa3133100a168573ed84d01e
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-22 00:20:52 -0300

    ♻️ refactor : Alterando variáveis para padronizamento

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/dto/PixTransactionRequestDto.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/dto/PixTransactionRequestDto.java
@@ -11,7 +11,7 @@ import java.math.BigDecimal;
 public class PixTransactionRequestDto {
 
     private String destinationKeyValue;
-    private Long idClient;
+    private Long originClientId;
     private BigDecimal amount;
     private String description;
 }
