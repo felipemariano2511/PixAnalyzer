4214504df24e96d97cce1240e049f3916da72993
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-22 00:28:02 -0300

    ♻️ refactor : Alterando os dados utilizados para o treinamento da IA

--- java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/application/services/PixTransactionAiAnalyzeCnpj.java
+++ java-service/PixKeyAnalyser/src/main/java/br/com/unicuritiba/pixanalyser/application/services/PixTransactionAiAnalyzeCnpj.java
@@ -2,16 +2,21 @@ package br.com.unicuritiba.pixanalyser.application.services;
 
 import br.com.unicuritiba.pixanalyser.AiAnalyzeCnpjRequest;
 import br.com.unicuritiba.pixanalyser.AiAnalyzeCnpjResponse;
-import br.com.unicuritiba.pixanalyser.domain.models.PixTransaction;
+import br.com.unicuritiba.pixanalyser.domain.repositories.PixTransactionRepository;
+import br.com.unicuritiba.pixanalyser.dto.AiAnalyzeRequestDto;
 import br.com.unicuritiba.pixanalyser.dto.DictApiResponseDto;
 import br.com.unicuritiba.pixanalyser.dto.ReceitaFederalCnpjResponseDto;
 import br.com.unicuritiba.pixanalyser.integrations.ai.AiAnalyzeCnpjGrpcClient;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Service
 public class PixTransactionAiAnalyzeCnpj {
+
+    @Autowired
+    private PixTransactionRepository pixTransactionRepository;
 
     private final AiAnalyzeCnpjGrpcClient grpcClient;
 
@@ -21,45 +26,48 @@ public class PixTransactionAiAnalyzeCnpj {
 
     public AiAnalyzeCnpjResponse analyzeCnpj(DictApiResponseDto dictData,
                                          ReceitaFederalCnpjResponseDto receitaData,
-                                         PixTransaction transaction) {
-
-        System.out.println("⚙️ Chamando método analyzeCnpj");
-
-        System.out.println("🔍 receitaData.getShareCapital() = " + receitaData.getShareCapital());
-        System.out.println("🔍 dictData.getAccount() = " + dictData.getAccount());
-        System.out.println("🔍 dictData.getOwner() = " + dictData.getOwner());
-
-        AiAnalyzeCnpjRequest request = AiAnalyzeCnpjRequest.newBuilder()
-                .setId(UUID.randomUUID().toString())
-                .setOriginClientId(Math.toIntExact(transaction.getOriginClientId()))
-                .setDestinationKeyValue(transaction.getDestinationKeyValue())
-                .setAmount(transaction.getAmount().doubleValue())
-                .setTimestamp(transaction.getTimestamp().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME))
-                .setKey(transaction.getDestinationKeyValue())
-                .setKeyType(dictData.getKeyType())
-                .setInstitution(dictData.getInstitution())
-                .setBranch(dictData.getAccount().getBranch())
-                .setAccountNumber(dictData.getAccount().getNumber())
-                .setAccountType(dictData.getAccount().getType())
-                .setOwnerType(dictData.getOwner().getType())
-                .setOwnerName(dictData.getOwner().getName())
-                .setCnpj(receitaData.getCnpj())
-                .setCompanyName(receitaData.getCompanyName())
-                .setRegistrationDate(receitaData.getRegistrationDate())
-                .setStatus(receitaData.getStatus())
-                .setStatusDate(receitaData.getStatusDate())
-                .setShareCapital(Double.parseDouble(receitaData.getShareCapital()))
-                .setBranchType(receitaData.getBranchType())
-                .setCommonTransfersClient(5)
-                .setAllTransfers(50337)
-                .build();
-
+                                         AiAnalyzeRequestDto aiAnalyzeRequestDto) {
         try {
-            return grpcClient.analyze(request);
+            AiAnalyzeCnpjRequest request = AiAnalyzeCnpjRequest.newBuilder()
+                    .setId(UUID.randomUUID().toString())
+                    .setOriginClientId(Math.toIntExact(aiAnalyzeRequestDto.getOriginClientId()))
+                    .setDestinationKeyValue(aiAnalyzeRequestDto.getDestinationKeyValue())
+                    .setAmount(aiAnalyzeRequestDto.getAmount().doubleValue())
+                    .setTimestamp(String.valueOf(LocalDateTime.now()))
+                    .setKey(aiAnalyzeRequestDto.getDestinationKeyValue())
+                    .setKeyType(dictData.getKeyType())
+                    .setInstitution(dictData.getInstitution())
+                    .setBranch(dictData.getAccount().getBranch())
+                    .setAccountNumber(dictData.getAccount().getNumber())
+                    .setAccountType(dictData.getAccount().getType())
+                    .setOwnerType(dictData.getOwner().getType())
+                    .setOwnerName(dictData.getOwner().getName())
+                    .setCnpj(receitaData.getCnpj())
+                    .setCompanyName(receitaData.getCompanyName())
+                    .setRegistrationDate(receitaData.getRegistrationDate())
+                    .setStatus(receitaData.getStatus())
+                    .setStatusDate(receitaData.getStatusDate())
+                    .setShareCapital(Double.parseDouble(receitaData.getShareCapital()))
+                    .setBranchType(receitaData.getBranchType())
+                    .setCommonTransfersClient(countCommonTransfers(aiAnalyzeRequestDto.getOriginClientId(), aiAnalyzeRequestDto.getDestinationKeyValue()))
+                    .setAllTransfers(countAllTransfers(aiAnalyzeRequestDto.getDestinationKeyValue()))
+                    .build();
+
+                return grpcClient.analyze(request);
+
         } catch (Exception e) {
             System.err.println("❌ Erro ao chamar IA via gRPC: " + e.getMessage());
             e.printStackTrace();
             throw new RuntimeException("Erro ao chamar serviço de IA", e);
         }
     }
+
+    public Integer countCommonTransfers(Long id, String key) {
+        return pixTransactionRepository.countByOriginClientIdAndDestinationKeyValue(id, key);
+    }
+
+    public Integer countAllTransfers(String key) {
+        return pixTransactionRepository.countByDestinationKeyValue(key);
+    }
+
 }
