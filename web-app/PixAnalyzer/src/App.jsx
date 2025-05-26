75a4c6aa735dca6c01f81486b26bda2b88af8db4
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-05-26 14:26:20 -0300

    ✨ feat : Incluindo projeto FrontEnd da aplicação

---
+++ web-app/PixAnalyzer/src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import PixInputScreen from "../src/components/PixInputScreen/PixInputScreen";
+import PixTransactionScreen from "../src/components/PixTransactionScreen/PixTransactionScreen";
+import PixAccountScreen from "../src/components/PixAccountScreen/PixAccountScreen";
+
+function App() {
+  return (
+    <Router basename="/web-app">
+      <Routes>
+        <Route path="/" element={<PixInputScreen />} />
+        <Route path="/valor" element={<PixTransactionScreen />} />
+        <Route path="/conta" element={<PixAccountScreen />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
