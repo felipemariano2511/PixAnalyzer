0f52cd1e20d2637ea8ba3ad3dffe9e1ca0cf9ca8
Felipe Mariano <136535564+felipemariano2511@users.noreply.github.com>
2025-06-17 02:43:26 -0300

    ✨ feat : Melhorias e novas telas no Front End

--- web-app/PixAnalyzer/src/components/PixSuccessfulScreen/PixSuccessfulScreen.jsx
+++ web-app/PixAnalyzer/src/components/PixSuccessfulScreen/PixSuccessfulScreen.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import { AiOutlineCheckCircle } from "react-icons/ai";
 import { Link } from "react-router-dom";
-import { IoIosArrowForward } from "react-icons/io";
+import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { IoHomeOutline, IoShareOutline } from "react-icons/io5";
 import { FaPix } from "react-icons/fa6";
-import { IoIosArrowBack } from "react-icons/io";
-import { IoHomeOutline } from "react-icons/io5";
-import { IoShareOutline } from "react-icons/io5";
+import { FcLock } from "react-icons/fc";
 import styles from "./PixSuccessfulScreen.module.css";
+import pixSuccessImage from "../../../public/money.png";
 
 function PixSuccessfulScreen() {
   const requestTransaction = JSON.parse(
@@ -24,15 +23,11 @@ function PixSuccessfulScreen() {
 
       <div className={styles["pix-content"]}>
         <div className={styles["check-area"]}>
-          <div className={styles["money-stack"]}>
-            <div className={`${styles.money} ${styles["money-1"]}`}></div>
-            <div className={`${styles.money} ${styles["money-2"]}`}></div>
-          </div>
-          <AiOutlineCheckCircle className={styles["check-icon"]} />
-          <div className={styles.coins}>
-            <div className={`${styles.coin} ${styles["coin-1"]}`}></div>
-            <div className={`${styles.coin} ${styles["coin-2"]}`}></div>
-          </div>
+          <img
+            src={pixSuccessImage}
+            alt="Pix concluído"
+            className={styles["pix-success-image"]}
+          />
         </div>
 
         <div className={styles["info-area"]}>
@@ -42,38 +37,35 @@ function PixSuccessfulScreen() {
           </p>
         </div>
 
-        <div className={styles["security-area"]}>
-          <p className={styles["security-text"]}>Seu Pix ainda mais</p>
-          <div className={styles["protected-area"]}>
-            <span className={styles["protected-text"]}>protegido</span>
+        <div className={styles["security-box"]}>
+          <div className={styles["color-bar"]}></div>
+          <div className={styles["security-content"]}>
+            <span className={styles["security-text"]}>
+              Seu Pix ainda mais protegido
+            </span>
+            <FcLock className={styles["security-icon"]} />
           </div>
         </div>
 
         <div className={styles["actions-area"]}>
-          <button className={styles["action-button"]}>
+          <Link to="/home" className={styles["action-button"]}>
             <IoShareOutline className={styles["action-icon"]} />
             <span className={styles["action-label"]}>
               Compartilhar comprovante
             </span>
             <IoIosArrowForward className={styles["action-arrow"]} />
-          </button>
-          <button className={styles["action-button"]}>
+          </Link>
+          <Link to="/home" className={styles["action-button"]}>
             <FaPix className={styles["pix-icon-container"]} />
             <span className={styles["action-label"]}>Fazer outro Pix</span>
             <IoIosArrowForward className={styles["action-arrow"]} />
-          </button>
-          <button className={styles["action-button"]}>
+          </Link>
+          <Link to="/home" className={styles["action-button"]}>
             <IoHomeOutline className={styles["action-icon"]} />
             <span className={styles["action-label"]}>Voltar ao início</span>
             <IoIosArrowForward className={styles["action-arrow"]} />
-          </button>
+          </Link>
         </div>
-      </div>
-
-      <div className={styles["navigation-dots"]}>
-        <div className={styles["nav-dot"]}></div>
-        <div className={`${styles["nav-dot"]} ${styles.active}`}></div>
-        <div className={styles["nav-dot"]}></div>
       </div>
     </div>
   );
